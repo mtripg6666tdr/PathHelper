caa90d9333ef091b3795327671731e4eef00f7c6
mtripg6666tdr <56076195+mtripg6666tdr@users.noreply.github.com>
2020-01-26 20:34:11 +0900

    Create Helper.cs

---
+++ Helper.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using System.IO;
+
+static class Helper
+{
+    /// <summary>
+    /// 絶対パスを返します。
+    /// </summary>
+    /// <param name="path">相対パス</param>
+    /// <returns>絶対パス</returns>
+    public static string GetFullPath(string path)
+    {
+        string ret = $@"{System.AppDomain.CurrentDomain.BaseDirectory.TrimEnd('\\')}\{path}";
+        return ret;
+    }
+
+    public static string GetExceptionString(Exception ex)
+    {
+        string ret = $"Threw a exception:{ex.Message}. StackTrace:{ex.StackTrace}\r\n";
+        return ret;
+    }
+
+    /// <summary>
+    /// 改行記号を統一します。
+    /// </summary>
+    /// <param name="NewLineSign">改行記号を指定します</param>
+    /// <returns>改行記号がパラメーターNewLineSignに指定された改行記号に統一されます</returns>
+    /// <see cref="NewLineSign"/>
+    /// <example>"Hello,\r\nWorld.".UnificateNewLineSign(NewLineSign.RN)</example>
+    public static string UnificateNewLineSine(this string str, string NewLineSign)
+    {
+        return str.Replace("\n\r", "\r\n").Replace("\r\n", "\r").Replace("\r", "\n").Replace("\n", NewLineSign);
+    }
+}
